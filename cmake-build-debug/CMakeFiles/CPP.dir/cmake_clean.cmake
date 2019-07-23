56c11a8c792d0a8356f8f6a63439297ad212a85e
Alexey Budaev <budaevaa@mail.ru>
2019-07-23 18:59:40 +0700

    переделал билд. Сборка будет происходить с верхнего уровня

---
+++ cmake-build-debug/CMakeFiles/CPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP.dir/Введение_в_программирование_(Яндекс)/Цикл_while/1.5.1.cpp.o"
+  "CPP.pdb"
+  "CPP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
